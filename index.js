db0d2c69034ee4b3d610c21c4a39dad87c4015ec
tonantr <tonantruong@hotmail.com>
2023-03-14 23:18:40 -0400

    complete Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let result;
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return result = "This one is on me!";
+  } else if (feet === 400 || feet <= 2000) {
+    return result = "That will be twenty bucks.";
+  } else if (feet > 2000 && feet <= 2500) {
+    return result = "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return result = "No can do.";
+  }
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
+};
+
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return result = "Thank you so much."
+  } else if (tip === "not as generous") {
+    return result = "Thank you."
+  } else if ("thanks for everything") {
+    return result = "Bye."
+  }
+};
+
